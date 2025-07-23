a3aecc39c9e835e4379a69d7c724f98f81e1b286
laudoluk <Laudo-Finda-Pedro.Lukoki@hsrw.org>
2025-07-23 19:06:36 +0200

    Update FieldRobotBlynk-esp32CAM.ino
    limiting total power of motors

--- FieldRobotBlynk-esp32CAM/FieldRobotBlynk-esp32CAM.ino
+++ FieldRobotBlynk-esp32CAM/FieldRobotBlynk-esp32CAM.ino
@@ -186,23 +186,23 @@ void loop() {
   Blynk.run();
 
   int pwmLeft, pwmRight;
-  float powerLimit = 0.6; // 60% of full power (change to anything from 0.0 to 1.0)
+  float powerLimit = 0.3; // 30% of full power (change to anything from 0.0 to 1.0)
 
   int offsetL = sliderValue - 2048;
   // Map offset range (-512 to +511) to ESC signal range (0–180)
   if (abs(offsetL) < 200) {
     pwmLeft = neutralSignal; // Deadzone
   } else {
-    pwmLeft = map(offsetL,-2048, 2048, -90, 90);
-    pwmLeft = neutralSignal + fullRange * powerLimit;
+    int fullLRange = map(offsetL, -2048, 2048, -90, 90);
+    pwmLeft = neutralSignal + fullLRange * powerLimit;
   }
 
   int offsetR = joystick_steer_pos - 2048;
   if (abs(offsetR) < 200) {
     pwmRight = neutralSignal; // Deadzone
   } else {
-    pwmRight = map(offsetR,-2048, 2048, -90, 90);
-    pwmRight = neutralSignal + fullRange * powerLimit;
+    int fullRRange = map(offsetR, -2048, 2048, -90, 90);
+    pwmRight = neutralSignal + fullRRange * powerLimit;
   }
 
   esc1.write(pwmLeft);
